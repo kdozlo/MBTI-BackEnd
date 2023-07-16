5b55d5004d24244b83849714dfadd7908ab46b77
kiryanchi <kiryanchi@naver.com>
2023-07-16 23:57:11 +0900

    Fix: CrossOrigin

--- src/main/java/com/gdsc/mbti/controller/MbtiController.java
+++ src/main/java/com/gdsc/mbti/controller/MbtiController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/mbti")
 @RequiredArgsConstructor
+@CrossOrigin
 public class MbtiController {
 
     private final MbtiService service;
